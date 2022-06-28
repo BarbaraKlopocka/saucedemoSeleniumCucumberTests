cc3776d01d8fdb7d9c73287d7d4b6d0140f1e113
Barbara Kłopocka <bklopocka@gmail.com>
2022-06-28 20:57:51 +0200

    fix3

--- src/test/java/com/company/Config.java
+++ src/test/java/com/company/Config.java
@@ -11,20 +11,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class Config {
-    @After
-    public void tearDown(Scenario scenario) {
-        if (scenario.isFailed()) {
-            TakesScreenshot scrShot = ((TakesScreenshot) DriverSingleton.getInstance());
-            File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
-            File DestFile = new File("screen.png");
 
-            try {
-                FileUtils.copyFile(SrcFile, DestFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
-        DriverSingleton.getInstance().quit();
-    }
 }
--- src/test/java/com/company/steps/MainPageSteps.java
+++ src/test/java/com/company/steps/MainPageSteps.java
@@ -20,43 +20,4 @@ public class MainPageSteps {
         inventoryPage = new InventoryPage();
     }
 
-    @Given("Go to the home page")
-    public void goToTheHomePage() {
-        mainPage.goToPage();
-    }
-
-    @And("User login with username {string} and password as {string}")
-    public void theUserProvidesTheUsernameAsAndPasswordAs(String username, String password) {
-        mainPage.login(username, password);
-    }
-
-    @Then("The user should login successfully and is on the inventory page")
-    public void theUserShouldLoginSuccessfullyAndIsOnTheInventoryPage() {
-        assertTrue(inventoryPage.browserIsOnThePage());
-    }
-
-    @Then("The user should login unsuccessfully and is not on the inventory page")
-    public void theUserShouldLoginUnsuccessfullyAndIsNotOnTheInventoryPage() {
-        assertFalse(inventoryPage.browserIsOnThePage());
-    }
-
-    @Given("Go to the menu and click logout button")
-    public void goToTheMenuAndClickLogoutButton() {
-        inventoryPage.logout();
-    }
-
-    @Then("The user is on home page")
-    public void theUserIsOnHomePage() {
-        assertTrue(mainPage.browserIsOnTheMainPage());
-    };
-
-    @And("Go to the menu")
-    public void goToTheMenu() {
-        inventoryPage.menuButton();
-    }
-
-    @Then("Menu should contain {int} references")
-    public void menuShouldContainReferences(int arg0) {
-        inventoryPage.referenceCheck();
-    }
 }
